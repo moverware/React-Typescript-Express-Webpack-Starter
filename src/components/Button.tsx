ee108ec03bbe02a6b9f2c4be6b14fcd586b0a383
Mitchell Overfield <t-miove@microsoft.com>
2018-08-20 14:58:54 -0700

    add better samples

---
+++ src/components/Button.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+export const Button: React.StatelessComponent<{}> = () => {
+    return <h1>Button</h1>
+}
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,16 +1,41 @@
 import * as React from 'react'
+import { Button } from './Button'
+
 interface HelloProps {
     compiler: string
     framework: string
     bundler: string
 }
-export class Hello extends React.Component<HelloProps, {}> {
+
+interface HelloState {
+    showButton: boolean
+}
+
+export class Hello extends React.Component<HelloProps, HelloState> {
+    constructor(props: HelloProps) {
+        super(props)
+        this.state = {
+            showButton: false
+        }
+    }
+
     render() {
-        return (
-            <h1>
-                This is a {this.props.framework} application using{' '}
-                {this.props.compiler} with {this.props.bundler}
-            </h1>
-        )
+        const elements: React.ReactElement<any>[] = []
+        if (this.state.showButton) {
+            elements.push(
+                <div>
+                    <Button />
+                </div>
+            )
+        } else {
+            elements.push(
+                <h1>
+                    This is a {this.props.framework} application using{' '}
+                    {this.props.compiler} with {this.props.bundler}
+                </h1>
+            )
+        }
+
+        return elements
     }
 }
